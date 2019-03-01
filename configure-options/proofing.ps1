0de255f1a5d36a15abe2b6f7a5409d0b6700a0fc
René Nyffenegger <mail@renenyffenegger.ch>
2019-03-01 16:13:30 +0100

    more autocorrect options

--- configure-options/proofing.ps1
+++ configure-options/proofing.ps1
@@ -7,22 +7,33 @@
 #
 
 $wrd = new-object -com word.application
-$wrd.visible = $true
+$wrd.visible = $false
 
 $opt = $wrd.options
+$auc = $wrd.autoCorrect
 
 
                                                              #   A u t o C o r r e c t   O p t i o n s
                                                              #   =====================================
                                                              #
+                                                             #   AutoCorrect
+                                                             #   --------------------------------------------------
+$auc.displayAutoCorrectOptions                    = $true    #   Show AutoCorrect Option buttons
+$auc.correctInitialCaps                           = $false   #   Correct TWo INitial CApitals
+$auc.correctSentenceCaps                          = $false   #   Capitalize first letter of sentences
+$auc.correctTableCells                            = $false   #   Capitalize first letter of table cells
+$auc.correctDays                                  = $true    #   Capitalize names of days
+$auc.correctCapsLock                              = $false   #   Correct accidental usage of cAPS LOCK key
+                                                             #
+                                                             #
                                                              #   Replace as you type
                                                              #   --------------------------------------------------
-$opt.autoFormatAsYouTypeReplaceQuotes             = $false   #  "Straight quotes" with “smart quotes“
-$opt.autoFormatAsYouTypeReplaceFractions          = $false   #   Fractions (1/2) with fraction character ½
-$opt.autoFormatAsYouTypeReplacePlainTextEmphasis  = $false   #  *Bold* and _italic_ with real formatting
+$opt.autoFormatAsYouTypeReplaceQuotes             = $true    #  "Straight quotes" with “smart quotes“
+$opt.autoFormatAsYouTypeReplaceFractions          = $true    #   Fractions (1/2) with fraction character ½
+$opt.autoFormatAsYouTypeReplacePlainTextEmphasis  = $true    #  *Bold* and _italic_ with real formatting
 $opt.autoFormatAsYouTypeReplaceHyperlinks         = $false   #   Internet and network paths with hyperlinks
-$opt.autoFormatAsYouTypeReplaceOrdinals           = $false   #   Ordinals (1st) with superscript
-$opt.autoFormatAsYouTypeReplaceSymbols            = $false   #   Hyphens (--) with dash
+$opt.autoFormatAsYouTypeReplaceOrdinals           = $true    #   Ordinals (1st) with superscript
+$opt.autoFormatAsYouTypeReplaceSymbols            = $true    #   Hyphens (--) with dash
                                                              #
                                                              #
                                                              #   Apply as you type
@@ -31,7 +42,7 @@ $opt.autoFormatAsYouTypeApplyBulletedLists        = $false   #   Automatic bulle
 $opt.autoFormatAsYouTypeApplyBorders              = $false   #   Border lines
 $opt.autoFormatAsYouTypeApplyHeadings             = $false   #   Built-in Heading styles
 $opt.autoFormatAsYouTypeApplyNumberedLists        = $false   #   Automatic numbered lists
-$opt.autoFormatAsYouTypeApplyNumberedLists        = $false   #   Tables
+$opt.autoFormatAsYouTypeApplyTables               = $false   #   Tables
                                                              #
                                                              #
                                                              #   Automatically as you type
@@ -48,7 +59,7 @@ $opt.autoFormatAsYouTypeDefineStyles              = $false   #   Define styles b
 $opt.ignoreUppercase                              = $false   #   Ignore words in UPPERCASE
 $opt.ignoreMixedDigits                            = $false   #   Ignore words that contain numbers
 $opt.ignoreInternetAndFileAddresses               = $false   #   Ignore Internet and file addresses
-$opt.repeatWord                                   = $false   #   Flag repeated words
+$opt.repeatWord                                   = $true    #   Flag repeated words
 $opt.allowAccentedUppercase                       = $false   #   Enforce accented uppercase in French
 $opt.suggestFromMainDictionaryOnly                = $false   #   Suggest from main dictionary only
                                                              #
@@ -56,9 +67,9 @@ $opt.suggestFromMainDictionaryOnly                = $false   #   Suggest from ma
                                                              #
                                                              #   W h e n   c o r r e c t i n g   s p e l l i n g   a n d   g r a m m a r
                                                              #   =======================================================================
-$opt.checkSpellingAsYouType                       = $false   #   Check-spelling as you type
-$opt.checkGrammarAsYouType                        = $false   #   Mark grammar errors as you type
-$opt.contextualSpeller                            = $false   #   Frequently confused words
+$opt.checkSpellingAsYouType                       = $true    #   Check-spelling as you type
+$opt.checkGrammarAsYouType                        = $true    #   Mark grammar errors as you type
+$opt.contextualSpeller                            = $true    #   Frequently confused words
 $opt.checkGrammarWithSpelling                     = $true    #   Check grammar with spelling
 #opt.????                                         = $false   #   Show readability statistics
 
